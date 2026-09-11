a0d6a714d61a716a5f7d48fb3908a6bc4faf20bb
aigenieai23-netizen <aigenie.ai23@gmail.com>
2026-09-11 17:43:53 +0100

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,12 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
 
-  if ENV['SMTP_ADDRESS']
+if ENV['RESEND_API_KEY']
+    config.action_mailer.delivery_method = :resend
+    config.action_mailer.resend_settings = { api_key: ENV.fetch('RESEND_API_KEY', nil) }
+  end
+
+    if ENV['SMTP_ADDRESS']
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       address: ENV.fetch('SMTP_ADDRESS', nil),
